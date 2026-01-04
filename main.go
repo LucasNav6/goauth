29fbde22a901391ec34e63178c662da58fe184cc
Lucas Navarro <lucasnav6@gmail.com>
2026-01-04 19:41:42 -0300

    feat: implement user authentication methods and password utilities

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package goauth
 
-import "github.com/LucasNav6/goauth/models"
+import (
+	"github.com/LucasNav6/goauth/models"
+	entites "github.com/LucasNav6/goauth/models/entities"
+	"github.com/LucasNav6/goauth/providers"
+)
 
 func CreateConfiguration() *models.Configuration {
 	return &models.Configuration{}
+}
+
+func SignUpWithEmailAndPassword(cfg *models.Configuration, user entites.User, password string) (error, *entites.Account) {
+	return providers.SignUpWithEmailAndPassword(cfg, user, password)
+}
+
+func SignInWithEmailAndPassword(cfg *models.Configuration, email string, password string) (error, *entites.User, *entites.Session) {
+	return providers.SignInWithEmailAndPassword(cfg, email, password)
+}
+
+func ResetPasswordWithEmailAndPassword(cfg *models.Configuration, email string, oldPassword string, newPassword string) error {
+	return providers.ResetPasswordWithEmailAndPassword(cfg, email, oldPassword, newPassword)
+}
+
+func SignUpWithMagicLink(cfg *models.Configuration, user entites.User) (error, *entites.Account) {
+	return providers.SignUpWithMagicLink(cfg, user)
+}
+
+func SignInWithMagicLink(cfg *models.Configuration, email string, token string, expirationInSeconds int64) (error, *entites.Session) {
+	return providers.SignInWithMagicLink(cfg, email, token, expirationInSeconds)
+}
+
+func ValidateMagicLinkSession(cfg *models.Configuration, token string) (error, *entites.Session) {
+	return providers.ValidateMagicLinkSession(cfg, token)
 }
--- utilities/hash.go
+++ utilities/hash.go
@@ -1,4 +1,13 @@
 package utilities
 
-func HashPassword(password string) string          {}
-func CheckPasswordHash(password, hash string) bool {}
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) string {
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes)
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
